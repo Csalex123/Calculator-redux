45470716a83b387514a2acc72e7b5578a8321990
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-10-05 23:46:03 -0300

    Connect redux old and userSelect redux with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import './App.css';
+
+/* Forma atual de store com hooks */
 
 function App() {
+  const result = useSelector((state) => {
+    return state.calculator;
+  });
+
   return (
     <div className="App">
-    dsad
+      <input type="text" placeholder="Valor 1" />
+      <input type="text" placeholder="Valor 2" />
+      <button>Calcular</button>
+
+      <div className="result">
+        Resultado: {result}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/* ----- Forma antiga de chamar o store do redux ------ */
+
+// import { connect } from 'react-redux';
+
+// function App({ result }) {
+//   return (
+//     <div className="App">
+//       <input type="text" placeholder="Valor 1" />
+//       <input type="text" placeholder="Valor 2" />
+//       <button>Calcular</button>
+
+//       <div className="result">
+//         Resultado: {result}
+//       </div>
+//     </div>
+//   );
+// }
+
+// function mapStateToProps(state) {
+//   return {
+//     result: state.calculator
+//   }
+// }
+
+// export default connect(mapStateToProps)(App);
