0b0a3e3e9d2341577e7b6886f1af1e409e5f86f5
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-10-06 00:11:35 -0300

    Sum and substract

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { subtract, sum } from './store/Calculator/Calculator.actions';
 
 import './App.css';
 
 /* Forma atual de store com hooks */
 
+/* Hooks 
+
+- useSelector: ele pega o estado da entidade
+- useDispatch: envie uma action pro reduce pra atualizar o estado
+
+*/
+
 function App() {
-  const result = useSelector((state) => {
+  const [typeCalculator, setTypeCalculator] = useState(0);
+  const [values, setValues] = useState({
+    a: 0,
+    b: 0
+  });
+
+  const dispatch = useDispatch();
+
+  const result = useSelector(state => {
     return state.calculator;
   });
 
+  const onChangeForm = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  /* Disparando uma ação pro reduce */
+  const handleSendSum = () => {
+    const { a, b } = values;
+
+    dispatch(typeCalculator === 0 ? sum(parseInt(a), parseInt(b)) : subtract(parseInt(a), parseInt(b)));
+
+  }
+
   return (
     <div className="App">
-      <input type="text" placeholder="Valor 1" />
-      <input type="text" placeholder="Valor 2" />
-      <button>Calcular</button>
+      <select onChange={(event) => setTypeCalculator( parseInt(event.target.value) )}>
+        <option value="0">Somar</option>
+        <option value="1">Diminuir</option>
+      </select>
+      <input type="text" name="a" value={values.a} placeholder="Valor 1" onChange={onChangeForm} />
+      <input type="text" name="b" value={values.b} placeholder="Valor 2" onChange={onChangeForm} />
+      <button onClick={() => handleSendSum(1, 2)}>Calcular</button>
 
       <div className="result">
         Resultado: {result}
